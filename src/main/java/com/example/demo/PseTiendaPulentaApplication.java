46a200d2c237df8e3a22083a2083903e959834db
POAPCA02LC0820 <SSDD@POAPCA02LC0820.duoc.acad>
2025-11-06 16:44:47 -0300

    api springboot pseTienda

---
+++ src/main/java/com/example/demo/PseTiendaPulentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseTiendaPulentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseTiendaPulentaApplication.class, args);
+	}
+
+}
